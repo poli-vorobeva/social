aa75b95dc46aa155c194740ad0edc79fcda13f8e
poli8512 <poli8512@gmail.com>
2023-05-08 10:00:00 +0300

    init project

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const start=async()=>{
+  try{
+    const PORT = process.env.PORT ||5000
+    const app = await NestFactory.create(AppModule)
+    await app.listen(PORT,()=>console.log(`Server Listen port ${PORT}`))
+  }catch(e){
+    console.log(e)
+  }
+}
+start()
---
+++ src/user/user.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from "@nestjs/common";
+
+@Injectable()
+export class UserService{
+
+	async create(){
+		//create
+	}
+	async getUser(){
+
+	}
+}
